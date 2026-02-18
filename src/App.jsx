6a2231471c8c8da4917302c105aaeb92f7b8734b
deuxon <info@deuxon.com>
2026-02-18 22:25:17 +0100

    Create src/App.jsx file with main application component

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Welcome to Deck Planner!</h1>
+            {/* Add your components and logic here */}
+        </div>
+    );
+};
+
+export default App;
