f9f93a79c03f5b8d51e4d795ba0129ddf8ee7da0
deuxon <info@deuxon.com>
2026-02-18 23:19:47 +0100

    Create src/main.jsx file with React 18 entry point code

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './App';
 
-const rootElement = document.getElementById('root');
-const root = ReactDOM.createRoot(rootElement);
-
-root.render(<h1>Hello, World!</h1>);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
