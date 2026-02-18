dc784c7b0baf2bb92a5a58d506069c1a9876031c
deuxon <info@deuxon.com>
2026-02-18 22:40:54 +0100

    Add Vite configuration file

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,12 @@
 // vite.config.js
 import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [react()],
   server: {
-    port: 3000,
     open: true,
-  },
-  build: {
-    outDir: 'dist',
+    port: 3000,
   },
 });
