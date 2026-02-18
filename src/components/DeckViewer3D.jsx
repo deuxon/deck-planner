9f58e32e71219a667cfd1567ba694b60641e604a
deuxon <info@deuxon.com>
2026-02-18 22:30:01 +0100

    Create DeckViewer3D component

---
+++ src/components/DeckViewer3D.jsx
@@ -0,0 +1,13 @@
+// DeckViewer3D component
+import React from 'react';
+
+const DeckViewer3D = () => {
+    return (
+        <div>
+            <h1>Deck Viewer 3D</h1>
+            {/* Your 3D deck viewing implementation goes here */}
+        </div>
+    );
+};
+
+export default DeckViewer3D;
