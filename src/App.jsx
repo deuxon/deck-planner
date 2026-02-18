3b7569549ea7c45063293bb3030dce64019a047d
deuxon <info@deuxon.com>
2026-02-19 00:10:36 +0100

    Update App.jsx with new content

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import Header from './components/Header';
 import Sidebar from './components/Sidebar';
 import MainContent from './components/MainContent';
+import './App.css';
 
 const App = () => {
     return (
         <div className="app">
             <Header />
-            <div className="layout">
+            <div style={{ display: 'flex', flex: 1 }}>
                 <Sidebar />
                 <MainContent />
             </div>
